304b2f9da5ad3e17d90032c96362d9a9d861bd8c
Carlos Garcia <gtxcarl9@gmail.com>
2021-10-18 15:50:52 +0100

    index main file added

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Auth from '@/components/Auth/SignUp'
+
+const Main: NextPage = () => {
+  return (
+    <>
+      <Auth />
+    </>
+  )
+}
+
+export default Main
