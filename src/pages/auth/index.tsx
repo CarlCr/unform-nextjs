08ee13a18e064550614677c90d8bf0ee0a9daacd
Carlos Garcia <gtxcarl9@gmail.com>
2021-10-18 15:53:12 +0100

    Auth component added in pages

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import SignUpMain from '@/components/Auth/SignUp'
+
+const SignUp: NextPage = () => {
+  return (
+    <>
+      <SignUpMain />
+    </>
+  )
+}
+
+export default SignUp
